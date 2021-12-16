24fc814414e990b6e580fad69c588a37311c0d38
Yogendra <you@example.com>
2021-12-16 17:33:01 +0530

    ADDED : Display Hello World Message

--- HelloWorldApplication/src/main/java/com/example/demo/HelloWorldApplication.java
+++ HelloWorldApplication/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+	
+	/*
+	 * @SpringBootApplication:describs the start of spring project.And display hello world
+	 */
 
 	public static void main(String[] args) {
 		System.out.println("Hello World from Bridgelabz!!");
